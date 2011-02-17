3862ce15fdacab9e5565fa3a97df3a2d6c5ef7be
TomyLobo <tomylobo@nurfuerspam.de>
2011-02-17 13:21:41 +0000

    Added "/gonether old", which brings you to the old pre-fix Nether coordinates.

--- src/main/java/com/bukkit/doridian/yiffbukkit/commands/GoNetherCommand.java
+++ src/main/java/com/bukkit/doridian/yiffbukkit/commands/GoNetherCommand.java
@@ -1,5 +1,6 @@
 package com.bukkit.doridian.yiffbukkit.commands;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import com.bukkit.doridian.yiffbukkit.YiffBukkit;
@@ -8,15 +9,22 @@ public class GoNetherCommand extends ICommand {
 	public int GetMinLevel() {
 		return 3;
 	}
-	
+
 	public GoNetherCommand(YiffBukkit plug) {
 		plugin = plug;
 	}
 
 	public void Run(Player ply, String[] args, String argStr) {
-		ply.teleportTo(plugin.TogglePlayerWorlds(ply, ply.getLocation()));
+		if (argStr == "old") {
+			Location pos = ply.getLocation().clone();
+			pos.setZ(0);
+			ply.teleportTo(plugin.TogglePlayerWorlds(ply, pos));
+		}
+		else {
+			ply.teleportTo(plugin.TogglePlayerWorlds(ply, ply.getLocation()));
+		}
 	}
-	
+
 	public String GetHelp() {
 		return "Toggles between nether and normal";
 	}
