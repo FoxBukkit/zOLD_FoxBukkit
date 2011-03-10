ccbd2c17bfc25c63dcc20074b8e182299667f404
TomyLobo <tomylobo@nurfuerspam.de>
2011-03-10 21:45:51 +0000

    Added on/off parameters to /vanish.

--- src/main/java/de/doridian/yiffbukkit/commands/VanishCommand.java
+++ src/main/java/de/doridian/yiffbukkit/commands/VanishCommand.java
@@ -7,6 +7,7 @@ import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 import de.doridian.yiffbukkit.YiffBukkit;
+import de.doridian.yiffbukkit.YiffBukkitCommandException;
 
 public class VanishCommand extends ICommand {
 	public VanishCommand(YiffBukkit plug) {
@@ -19,24 +20,35 @@ public class VanishCommand extends ICommand {
 	}
 
 	@Override
-	public void Run(Player ply, String[] args, String argStr) {
+	public void Run(Player ply, String[] args, String argStr) throws YiffBukkitCommandException {
 		String playerName = ply.getName();
 		if (playerHelper.vanishedPlayers.contains(playerName)) {
+			if (argStr == "on")
+				throw new YiffBukkitCommandException("Already visible!");
+
 			playerHelper.vanishedPlayers.remove(playerName);
 			playerHelper.sendPacketToPlayersAround(ply.getLocation(), 1024, new Packet29DestroyEntity(ply.getEntityId()), ply, 3);
 			playerHelper.sendPacketToPlayersAround(ply.getLocation(), 1024, new Packet20NamedEntitySpawn(((CraftPlayer)ply).getHandle()), ply, 3);
 			playerHelper.SendServerMessage(ply.getName() + " reappeared.", playerHelper.GetPlayerLevel(ply));
 		}
 		else {
+			if (argStr == "off")
+				throw new YiffBukkitCommandException("Already invisible!");
+
 			playerHelper.vanishedPlayers.add(playerName);
 			playerHelper.sendPacketToPlayersAround(ply.getLocation(), 1024, new Packet29DestroyEntity(ply.getEntityId()), ply, 3);
 			playerHelper.SendServerMessage(ply.getName() + " vanished.", playerHelper.GetPlayerLevel(ply));
 		}
-		
+
 	}
 
 	@Override
 	public String GetHelp() {
 		return "Makes you invisible";
+	}
+
+	@Override
+	public String GetUsage() {
+		return "[on|off]";
 	}
 }
