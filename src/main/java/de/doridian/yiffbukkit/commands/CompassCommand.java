e05eb4caa33dacf7b4421fa984cdd3c6a2e81a7e
TomyLobo <tomylobo@nurfuerspam.de>
2011-04-17 17:07:40 +0000

    Fixed Made /compass wrap again and converted it to the latest command system

--- src/main/java/de/doridian/yiffbukkit/commands/CompassCommand.java
+++ src/main/java/de/doridian/yiffbukkit/commands/CompassCommand.java
@@ -2,24 +2,15 @@ package de.doridian.yiffbukkit.commands;
 
 import org.bukkit.entity.Player;
 
-import de.doridian.yiffbukkit.YiffBukkitPlayerListener;
 import de.doridian.yiffbukkit.util.Utils;
+import de.doridian.yiffbukkit.commands.ICommand.*;
 
+@Names("compass")
+@Help("Gives you your current bearing")
+@Level(0)
 public class CompassCommand extends ICommand {
-	public int GetMinLevel() {
-		return 0;
-	}
-
-	public CompassCommand(YiffBukkitPlayerListener playerListener) {
-		super(playerListener);
-	}
-
 	public void Run(Player ply, String[] args, String argStr) {
 		float yaw = ply.getLocation().getYaw();
-		playerHelper.SendDirectedMessage(ply, "Direction: "+Utils.yawToDirection(yaw)+" ("+Math.round(yaw)+")");
-	}
-
-	public String GetHelp() {
-		return "Gives you your current bearing";
+		playerHelper.SendDirectedMessage(ply, "Direction: "+Utils.yawToDirection(yaw)+" ("+Math.round((yaw+720)%360)+")");
 	}
 }
