49bca32577f2f5978fe8aaae531fad0899f1c0af
TomyLobo <tomylobo@nurfuerspam.de>
2011-05-08 23:07:02 +0000

    Added some parameters to /compass, that will work as soon as notch unfails the compass renderer.

--- src/main/java/de/doridian/yiffbukkit/commands/CompassCommand.java
+++ src/main/java/de/doridian/yiffbukkit/commands/CompassCommand.java
@@ -1,17 +1,62 @@
 package de.doridian.yiffbukkit.commands;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+import de.doridian.yiffbukkit.PermissionDeniedException;
+import de.doridian.yiffbukkit.YiffBukkitCommandException;
 import de.doridian.yiffbukkit.util.Utils;
+import de.doridian.yiffbukkit.warp.WarpDescriptor;
 import de.doridian.yiffbukkit.commands.ICommand.*;
 
 @Names("compass")
 @Help("Gives you your current bearing")
+@Usage("[spawn|home|here|player <name>|warp <name>]")
 @Level(0)
 public class CompassCommand extends ICommand {
 	@Override
-	public void Run(Player ply, String[] args, String argStr) {
-		float yaw = ply.getLocation().getYaw();
-		playerHelper.SendDirectedMessage(ply, "Direction: "+Utils.yawToDirection(yaw)+" ("+Math.round((yaw+720)%360)+")");
+	public void Run(Player ply, String[] args, String argStr) throws YiffBukkitCommandException {
+		if (args.length == 0) {
+			float yaw = ply.getLocation().getYaw();
+			playerHelper.SendDirectedMessage(ply, "Direction: "+Utils.yawToDirection(yaw)+" ("+Math.round((yaw+720)%360)+")");
+			return;
+		}
+
+		final Location location;
+		if (args[0].equals("spawn")) {
+			location = ply.getWorld().getSpawnLocation();
+		}
+		else if (args[0].equals("home")) {
+			location = playerHelper.GetPlayerHomePosition(ply);
+		}
+		else if (args[0].equals("here")) {
+			location = ply.getLocation();
+		}
+		else if (args[0].equals("player")) {
+			if (args.length < 2)
+				throw new YiffBukkitCommandException("Expected player name");
+
+			Player target = playerHelper.MatchPlayerSingle(args[1]);
+
+			if (!playerHelper.CanTp(ply, target))
+				throw new PermissionDeniedException();
+
+			location = target.getLocation();
+		}
+		else if (args[0].equals("warp")) {
+			if (args.length < 2)
+				throw new YiffBukkitCommandException("Expected warp name");
+
+			WarpDescriptor warpDescriptor = plugin.warpEngine.getWarp(ply.getName(), args[1]);
+
+			location = warpDescriptor.location;
+		}
+		else {
+			throw new YiffBukkitCommandException("Unrecognised parameter");
+		}
+
+		ply.setCompassTarget(new Location(location.getWorld(), location.getX()*16, location.getY()*16, location.getZ()*16));
+
+		playerHelper.SendDirectedMessage(ply, String.format("Set your compass target to %d/%d/%d", location.getBlockX(), location.getBlockY(), location.getBlockZ()));
 	}
 }
