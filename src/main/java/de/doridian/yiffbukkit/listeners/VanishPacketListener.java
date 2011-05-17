b42c1eced0d0fa21cd14da9f7ddadf097d4aeb83
TomyLobo <tomylobo@nurfuerspam.de>
2011-05-17 20:26:19 +0000

    Made /vanish handle packets 5, 17, 22, 29, 38, 39 and 40 in addition to the current ones.

--- src/main/java/de/doridian/yiffbukkit/listeners/VanishPacketListener.java
+++ src/main/java/de/doridian/yiffbukkit/listeners/VanishPacketListener.java
@@ -11,11 +11,18 @@ import de.doridian.yiffbukkit.util.PlayerHelper;
 
 import net.minecraft.server.Entity;
 import net.minecraft.server.EntityPlayer;
+import net.minecraft.server.Packet17;
 import net.minecraft.server.Packet18ArmAnimation;
 import net.minecraft.server.Packet20NamedEntitySpawn;
+import net.minecraft.server.Packet22Collect;
 import net.minecraft.server.Packet28EntityVelocity;
+import net.minecraft.server.Packet29DestroyEntity;
 import net.minecraft.server.Packet30Entity;
 import net.minecraft.server.Packet34EntityTeleport;
+import net.minecraft.server.Packet38EntityStatus;
+import net.minecraft.server.Packet39AttachEntity;
+import net.minecraft.server.Packet40EntityMetadata;
+import net.minecraft.server.Packet5EntityEquipment;
 
 public class VanishPacketListener extends PacketListener {
 	private final YiffBukkit plugin;
@@ -25,14 +32,24 @@ public class VanishPacketListener extends PacketListener {
 		plugin = instance;
 		playerHelper = plugin.playerHelper;
 
+		/*for (int i = 0; i < 256; ++i) {
+			PacketListener.addPacketListener(true, i, this, plugin);
+		}*/
+		PacketListener.addPacketListener(true,  5, this, plugin);
+		PacketListener.addPacketListener(true, 17, this, plugin);
 		PacketListener.addPacketListener(true, 18, this, plugin);
 		PacketListener.addPacketListener(true, 20, this, plugin);
+		PacketListener.addPacketListener(true, 22, this, plugin);
 		PacketListener.addPacketListener(true, 28, this, plugin);
+		PacketListener.addPacketListener(true, 29, this, plugin);
 		PacketListener.addPacketListener(true, 30, this, plugin);
 		PacketListener.addPacketListener(true, 31, this, plugin);
 		PacketListener.addPacketListener(true, 32, this, plugin);
 		PacketListener.addPacketListener(true, 33, this, plugin);
 		PacketListener.addPacketListener(true, 34, this, plugin);
+		PacketListener.addPacketListener(true, 38, this, plugin);
+		PacketListener.addPacketListener(true, 39, this, plugin);
+		PacketListener.addPacketListener(true, 40, this, plugin);
 	}
 
 	private static final String nameFromEntityId(World world, int entityID) {
@@ -50,6 +67,16 @@ public class VanishPacketListener extends PacketListener {
 
 		String otherName;
 		switch (packetID) {
+		case 5:
+			Packet5EntityEquipment p5 = (Packet5EntityEquipment) packet;
+			otherName = nameFromEntityId(ply.getWorld(), p5.a);
+			break;
+
+		case 17: // Use Bed
+			Packet17 p17 = (Packet17) packet;
+			otherName = nameFromEntityId(ply.getWorld(), p17.a);
+			break;
+
 		case 18:
 			Packet18ArmAnimation p18 = (Packet18ArmAnimation) packet;
 			otherName = nameFromEntityId(ply.getWorld(), p18.a);
@@ -60,9 +87,19 @@ public class VanishPacketListener extends PacketListener {
 			otherName = p20.b;
 			break;
 
+		case 22:
+			Packet22Collect p22 = (Packet22Collect) packet;
+			otherName = nameFromEntityId(ply.getWorld(), p22.b);
+			break;
+
 		case 28:
 			Packet28EntityVelocity p28 = (Packet28EntityVelocity) packet;
 			otherName = nameFromEntityId(ply.getWorld(), p28.a);
+			break;
+
+		case 29:
+			Packet29DestroyEntity p29 = (Packet29DestroyEntity) packet;
+			otherName = nameFromEntityId(ply.getWorld(), p29.a);
 			break;
 
 		case 30:
@@ -78,7 +115,26 @@ public class VanishPacketListener extends PacketListener {
 			otherName = nameFromEntityId(ply.getWorld(), p34.a);
 			break;
 
+		case 38:
+			Packet38EntityStatus p38 = (Packet38EntityStatus) packet;
+			otherName = nameFromEntityId(ply.getWorld(), p38.a);
+			break;
+
+		case 39:
+			Packet39AttachEntity p39 = (Packet39AttachEntity) packet;
+			if (playerHelper.vanishedPlayers.contains(nameFromEntityId(ply.getWorld(), p39.b)))
+				return false;
+
+			otherName = nameFromEntityId(ply.getWorld(), p39.a);
+			break;
+
+		case 40:
+			Packet40EntityMetadata p40 = (Packet40EntityMetadata) packet;
+			otherName = nameFromEntityId(ply.getWorld(), p40.a);
+			break;
+
 		default:
+			removePacketListener(true, packetID, this);
 			return true;
 		}
 
