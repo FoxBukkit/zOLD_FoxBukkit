51bbe27cd1cf6e08ba4922d69674db440d9b67fa
TomyLobo <tomylobo@nurfuerspam.de>
2011-06-22 20:41:56 +0000

    /me text is now light gray.

--- src/main/java/de/doridian/yiffbukkit/commands/MeCommand.java
+++ src/main/java/de/doridian/yiffbukkit/commands/MeCommand.java
@@ -10,7 +10,7 @@ import de.doridian.yiffbukkit.commands.ICommand.*;
 public class MeCommand extends ICommand {
 	@Override
 	public void run(CommandSender commandSender, String[] args, String argStr) {
-		String message = "* "+playerHelper.getPlayerTag(commandSender) + commandSender.getDisplayName() + " " + argStr;
+		String message = "§7* "+playerHelper.getPlayerTag(commandSender) + commandSender.getDisplayName() + "§7 " + argStr;
 
 		final String conversationTarget = playerHelper.conversations.get(commandSender.getName());
 		if (conversationTarget == null) {
