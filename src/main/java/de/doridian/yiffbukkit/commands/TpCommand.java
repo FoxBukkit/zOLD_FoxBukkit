6e60eb7c3a06204d020a16b66c73605706c74b92
TomyLobo <tomylobo@nurfuerspam.de>
2011-08-03 20:43:38 +0000

    Added a -s flag to /tp, which makes it only show the teleport message to people with the yiffbukkit.vanish.see permission. This flag requires the yiffbukkit.vanish permission.

--- src/main/java/de/doridian/yiffbukkit/commands/TpCommand.java
+++ src/main/java/de/doridian/yiffbukkit/commands/TpCommand.java
@@ -3,16 +3,18 @@ package de.doridian.yiffbukkit.commands;
 import org.bukkit.entity.Player;
 
 import de.doridian.yiffbukkit.PermissionDeniedException;
-import de.doridian.yiffbukkit.util.PlayerFindException;
+import de.doridian.yiffbukkit.YiffBukkitCommandException;
 import de.doridian.yiffbukkit.commands.ICommand.*;
 
 @Names("tp")
-@Help("Teleports you to the specified user")
-@Usage("<name>")
+@Help("Teleports you to the specified user. -s teleports you silently.")
+@Usage("[-s] <name>")
 @Permission("yiffbukkit.teleport.tp")
+@BooleanFlags("s")
 public class TpCommand extends ICommand {
 	@Override
-	public void Run(Player ply, String[] args, String argStr) throws PlayerFindException, PermissionDeniedException {
+	public void Run(Player ply, String[] args, String argStr) throws YiffBukkitCommandException {
+		args = parseFlags(args);
 		Player otherply = playerHelper.matchPlayerSingle(args[0]);
 
 		String playerName = ply.getName();
@@ -21,10 +23,13 @@ public class TpCommand extends ICommand {
 		if (!playerHelper.canTp(ply, otherply))
 			throw new PermissionDeniedException();
 
+		if (booleanFlags.contains('s') && !plugin.permissionHandler.has(ply, "yiffbukkit.vanish"))
+			throw new PermissionDeniedException();
+
 		ply.teleport(otherply);
 
-		if (plugin.vanish.vanishedPlayers.contains(playerName)) {
-			playerHelper.sendServerMessage(playerName + " silently teleported to " + otherName, "yiffbukkit.vanish");
+		if (plugin.vanish.vanishedPlayers.contains(playerName) || booleanFlags.contains('s')) {
+			playerHelper.sendServerMessage(playerName + " silently teleported to " + otherName, "yiffbukkit.vanish.see");
 		}
 		else {
 			playerHelper.sendServerMessage(playerName + " teleported to " + otherName);
