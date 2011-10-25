368ce1cdf252703dcce5f8a90fb1d0e4f3627ac5
Doridian Draconia <mriq91@gmail.com>
2011-10-25 19:44:07 +0000

    A bit more work on the Chat Channel system :3

--- src/main/java/de/doridian/yiffbukkit/chat/ChatHelper.java
+++ src/main/java/de/doridian/yiffbukkit/chat/ChatHelper.java
@@ -1,6 +1,7 @@
 package de.doridian.yiffbukkit.chat;
 
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 import org.bukkit.entity.Player;
 
@@ -9,7 +10,12 @@ import de.doridian.yiffbukkit.YiffBukkit;
 import de.doridian.yiffbukkit.YiffBukkitCommandException;
 
 public class ChatHelper extends StateContainer {
+	//maps STRING (name) to CHATCHANNEL
 	public HashMap<String, ChatChannel> channels = new HashMap<String, ChatChannel>();
+	//maps PLAYER to CHATCHANNEL
+	public HashMap<String, ChatChannel> activeChannel = new HashMap<String, ChatChannel>();
+	
+	public final ChatChannel DEFAULT;
 	
 	public void joinChannel(Player player, ChatChannel channel) {
 		String plyname = player.getName().toLowerCase();
@@ -30,17 +36,17 @@ public class ChatHelper extends StateContainer {
 	}
 	
 	public ChatChannel addChannel(Player owner, String name, boolean overwrite) throws YiffBukkitCommandException {
-		name = name.toLowerCase();
+		String keyname = name.toLowerCase();
 		ChatChannel newChan;
-		if(channels.containsKey(name)) {
+		if(channels.containsKey(keyname)) {
 			if(overwrite) {
-				newChan = channels.get(name);
+				newChan = channels.get(keyname);
 			} else {
 				throw new YiffBukkitCommandException("Channel exists already!");
 			}
 		} else {
 			newChan = new ChatChannel(name);
-			channels.put(name, newChan);
+			channels.put(keyname, newChan);
 		}
 		newChan.owner = owner.getName().toLowerCase();
 		return newChan;
@@ -54,12 +60,46 @@ public class ChatHelper extends StateContainer {
 		}
 	}
 	
+	public ChatChannel getActiveChannel(Player ply) {
+		ChatChannel chan = activeChannel.get(ply.getName().toLowerCase());
+		if(chan == null) chan = DEFAULT;
+		
+		return chan;
+	}
+	
+	public void sendChat(Player ply, String msg) {
+		sendChat(ply, msg, null);
+	}
+	
+	public void sendChat(Player ply, String msg, ChatChannel chan) {
+		if(chan == null) chan = getActiveChannel(ply);
+		
+		for(Entry<String,Boolean> entry : chan.players.entrySet()) {
+			if(!entry.getValue()) continue;
+			
+			Player player = plugin.getServer().getPlayerExact(entry.getKey());
+			if(player == null) continue;
+			
+			if(getActiveChannel(player) == chan) {
+				player.sendRawMessage(msg);
+			} else {
+				player.sendRawMessage("§2[" + chan.name + "]§f " + msg);
+			}
+		}
+	}
 	
 	private static ChatHelper instance;
 	YiffBukkit plugin;
 	ChatHelper(YiffBukkit plug) {
 		plugin = plug;
 		instance = this;
+		
+		ChatChannel cc = null;
+		try {
+			cc = addChannel(null, "DEFAULT", true);
+		} catch(Exception e) { }
+		
+		DEFAULT = cc;
 	}
 	
 	public static ChatHelper getInstance() {
--- src/main/java/de/doridian/yiffbukkit/chat/ChatListener.java
+++ src/main/java/de/doridian/yiffbukkit/chat/ChatListener.java
@@ -9,16 +9,26 @@ import de.doridian.yiffbukkit.YiffBukkit;
 
 public class ChatListener extends PlayerListener {
 	ChatHelper helper;
-	
 	YiffBukkit plugin;
+	
 	public ChatListener(YiffBukkit plug) {
 		plugin = plug;
 		helper = new ChatHelper(plugin);
-		plug.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_CHAT, this, Priority.Highest, plugin);
+		plugin.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_CHAT, this, Priority.Highest, plugin);
 	}
 	
 	@Override
 	public void onPlayerChat(PlayerChatEvent event) {
+		if(true) return; //for now!
 		
+		if(event.isCancelled()) return;
+		
+		String msg = event.getMessage();
+		char fchar = msg.charAt(0);
+		if(fchar == '/' || fchar == '#') return;
+		
+		helper.sendChat(event.getPlayer(), msg);
+		
+		event.setCancelled(true);
 	}
 }
