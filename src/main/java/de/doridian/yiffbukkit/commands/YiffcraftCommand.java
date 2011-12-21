d43469f2542ef4a3fd865d8069df6d98203ecccd
Doridian Draconia <mriq91@gmail.com>
2011-12-21 21:33:54 +0100

    Can now send URL to YC client for command hinting

---
+++ src/main/java/de/doridian/yiffbukkit/commands/YiffcraftCommand.java
@@ -0,0 +1,17 @@
+package de.doridian.yiffbukkit.commands;
+
+import de.doridian.yiffbukkit.YiffBukkitCommandException;
+import org.bukkit.entity.Player;
+
+@ICommand.Names("yiffcraft")
+@ICommand.Help("Command used by the YC client")
+@ICommand.Level(0)
+public class YiffcraftCommand extends ICommand {
+	@Override
+	public void Run(Player ply, String[] args, String argStr) throws YiffBukkitCommandException {
+		if(argStr.equalsIgnoreCase("getcommands")) {
+			ply.sendRawMessage("\u00a7f\u00a75\u00a7d" + 'c' + "http://mc.doridian.de/yb_commands.txt");
+			ply.sendRawMessage("Sent URL");
+		}
+	}
+}
