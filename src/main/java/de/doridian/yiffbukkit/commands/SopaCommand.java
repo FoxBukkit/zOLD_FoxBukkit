9dc81c8134160a536649e62c541a61ba6841ee24
TomyLobo <tomylobo@nurfuerspam.de>
2012-01-19 19:15:17 +0100

    Improved SopaCommand.

--- src/main/java/de/doridian/yiffbukkit/commands/SopaCommand.java
+++ src/main/java/de/doridian/yiffbukkit/commands/SopaCommand.java
@@ -1,6 +1,5 @@
 package de.doridian.yiffbukkit.commands;
 
-import de.doridian.yiffbukkit.YiffBukkit;
 import de.doridian.yiffbukkit.YiffBukkitCommandException;
 import de.doridian.yiffbukkit.util.Utils;
 import org.bukkit.command.CommandSender;
@@ -15,62 +14,58 @@ import java.util.Random;
 @ICommand.Help(";3")
 @ICommand.Usage("[probability]")
 @ICommand.Permission("yiffbukkit.setsopa")
-public class SopaCommand extends ICommand {
+public class SopaCommand extends ICommand implements Listener {
 	@Override
 	public void run(CommandSender sender, String[] args, String argStr) throws YiffBukkitCommandException {
 		final float probability;
-		if(args.length < 1) {
-			if(listener.probability == 0.0F) {
+		if (args.length < 1) {
+			if(this.probability == 0.0F) {
 				probability = 0.5F;
 			} else {
 				probability = 0.0F;
 			}
-		} else {
+		}
+		else {
 			probability = Integer.parseInt(args[0]) / 100.0F;
 		}
 
-		if(probability > 1) {
-			listener.probability = 1;
-		} else if(probability < 0) {
-			listener.probability = 0;
-		} else {
-			listener.probability = probability;
+		if (probability > 1) {
+			this.probability = 1;
 		}
-		
-		plugin.playerHelper.sendDirectedMessage(sender, "SOPA probability = " + (int)(listener.probability * 100) + "%");
+		else if(probability < 0) {
+			this.probability = 0;
+		}
+		else {
+			this.probability = probability;
+		}
+
+		plugin.playerHelper.sendDirectedMessage(sender, "SOPA probability = " + (int)(this.probability * 100) + "%");
 	}
 
-	final private SopaChatListener listener;
 	public SopaCommand() {
-		listener = new SopaChatListener(YiffBukkit.instance);
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
-	
-	class SopaChatListener implements Listener {
-		Random random = new Random();
-		public float probability = 0.0F;
 
-		public SopaChatListener(YiffBukkit plugin) {
-			plugin.getServer().getPluginManager().registerEvents(this, plugin);
-		}
+	Random random = new Random();
+	public float probability = 0.0F;
 
-		@EventHandler(event = PlayerChatEvent.class, priority = EventPriority.LOWEST)
-		public void censorChat(PlayerChatEvent event) {
-			String[] words = event.getMessage().split(" +");
-			if(words.length < 1) return;
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void censorChat(PlayerChatEvent event) {
+		String[] words = event.getMessage().split(" +");
+		if(words.length < 1) return;
 
-			int iLast = -5;
-			for(int i = 0; i < words.length; i++) {
-				if(random.nextFloat() <= probability) {
-					if(i != iLast) {
-						words[i] = "\u00a7k" + words[i];
-					}
-					iLast = i + 1;
-				} else if(iLast == i) {
-					words[i] = "\u00a7f" + words[i];
+		int iLast = -5;
+		for (int i = 0; i < words.length; i++) {
+			if(random.nextFloat() <= probability) {
+				if(i != iLast) {
+					words[i] = "\u00a7k" + words[i];
 				}
+				iLast = i + 1;
+			} else if(iLast == i) {
+				words[i] = "\u00a7f" + words[i];
 			}
-
-			event.setMessage(Utils.concatArray(words, 0, ""));
 		}
+
+		event.setMessage(Utils.concatArray(words, 0, ""));
 	}
 }
