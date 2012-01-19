d04ae680aefe336dc01fada11c44a3ddec282241
TomyLobo <tomylobo@nurfuerspam.de>
2012-01-19 20:05:00 +0100

    Updated MCBansKeyListener to the new event system.

--- src/main/java/de/doridian/yiffbukkit/mcbans/MCBansKeyListener.java
+++ src/main/java/de/doridian/yiffbukkit/mcbans/MCBansKeyListener.java
@@ -1,7 +1,8 @@
 package de.doridian.yiffbukkit.mcbans;
 
 import de.doridian.yiffbukkit.YiffBukkit;
-import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.getspout.spoutapi.event.input.InputListener;
 import org.getspout.spoutapi.event.input.KeyReleasedEvent;
 import org.getspout.spoutapi.keyboard.Keyboard;
@@ -11,10 +12,10 @@ public class MCBansKeyListener extends InputListener {
 
 	public MCBansKeyListener(YiffBukkit plug) {
 		plugin = plug;
-		plugin.getServer().getPluginManager().registerEvent(Event.Type.CUSTOM_EVENT, this, Event.Priority.Monitor, plugin);
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
 
-	@Override
+	@EventHandler(priority = EventPriority.MONITOR)
 	public void onKeyReleasedEvent(KeyReleasedEvent event) {
 		if(event.getKey().equals(Keyboard.KEY_F9)) {
 			new MCBansGUI(plugin, event.getPlayer());
