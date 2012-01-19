5bb34da1b8a73913a4a436572062088cf3d86006
TomyLobo <tomylobo@nurfuerspam.de>
2012-01-19 20:03:57 +0100

    Updated ChatScreenListener to the new event system and improved it a bit.

--- src/main/java/de/doridian/yiffbukkit/chat/ChatScreenListener.java
+++ src/main/java/de/doridian/yiffbukkit/chat/ChatScreenListener.java
@@ -1,32 +1,32 @@
 package de.doridian.yiffbukkit.chat;
 
 import de.doridian.yiffbukkit.YiffBukkit;
-import org.bukkit.event.Event;
-import org.bukkit.event.Event.Priority;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.getspout.spoutapi.event.screen.ScreenCloseEvent;
-import org.getspout.spoutapi.event.screen.ScreenListener;
 import org.getspout.spoutapi.event.screen.ScreenOpenEvent;
 import org.getspout.spoutapi.gui.ScreenType;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 import java.util.HashMap;
 
-public class ChatScreenListener extends ScreenListener {
+public class ChatScreenListener implements Listener {
 	YiffBukkit plugin;
 	HashMap<String, ChatPopup> popups = new HashMap<String, ChatPopup>();
-	
+
 	public ChatScreenListener(YiffBukkit plug) {
 		plugin = plug;
-		plugin.getServer().getPluginManager().registerEvent(Event.Type.CUSTOM_EVENT, this, Priority.Highest, plugin);
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
-	
+
 	public void removePopupFor(SpoutPlayer ply) {
 		String plyname = ply.getName().toLowerCase();
 		try {
 			ply.getMainScreen().removeWidget(popups.remove(plyname));
 		} catch(Exception e) { }
 	}
-	
+
 	public void makePopupFor(SpoutPlayer ply) {
 		String plyname = ply.getName().toLowerCase();
 		removePopupFor(ply);
@@ -34,7 +34,7 @@ public class ChatScreenListener extends ScreenListener {
 		newPop.init();
 		popups.put(plyname, newPop);
 	}
-	
+
 	public ChatPopup getPopupFor(SpoutPlayer ply) {
 		String plyname = ply.getName().toLowerCase();
 		if(!popups.containsKey(plyname)) {
@@ -42,20 +42,24 @@ public class ChatScreenListener extends ScreenListener {
 		}
 		return popups.get(plyname);
 	}
-	
-	@Override
-    public void onScreenOpen(ScreenOpenEvent event) {
-		if(event.getScreenType() != ScreenType.CHAT_SCREEN) return;
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onScreenOpen(ScreenOpenEvent event) {
+		if (event.getScreenType() != ScreenType.CHAT_SCREEN)
+			return;
+
 		SpoutPlayer ply = event.getPlayer();
-		ply.getMainScreen().attachWidget(plugin, getPopupFor(event.getPlayer()));
-    }
-       
-    @Override
-    public void onScreenClose(ScreenCloseEvent event) {
-    	if(event.getScreenType() != ScreenType.CHAT_SCREEN) return;
-    	SpoutPlayer ply = event.getPlayer();
-    	if(ply.getMainScreen().getActivePopup() instanceof ChatPopup) {
-    		ply.getMainScreen().closePopup();
-    	}
-    }
+		ply.getMainScreen().attachWidget(plugin, getPopupFor(ply));
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onScreenClose(ScreenCloseEvent event) {
+		if (event.getScreenType() != ScreenType.CHAT_SCREEN)
+			return;
+
+		SpoutPlayer ply = event.getPlayer();
+		if(ply.getMainScreen().getActivePopup() instanceof ChatPopup) {
+			ply.getMainScreen().closePopup();
+		}
+	}
 }
