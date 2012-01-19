a1e6984b1320f1f937869e39fd5b0391c3b50934
TomyLobo <tomylobo@nurfuerspam.de>
2012-01-19 18:30:45 +0100

    Updated TrapSheep to new event system.

--- src/main/java/de/doridian/yiffbukkit/sheep/TrapSheep.java
+++ src/main/java/de/doridian/yiffbukkit/sheep/TrapSheep.java
@@ -3,21 +3,40 @@ package de.doridian.yiffbukkit.sheep;
 import de.doridian.yiffbukkit.YiffBukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Sheep;
-import org.bukkit.event.Event.Priority;
-import org.bukkit.event.Event.Type;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-import org.bukkit.event.entity.EntityListener;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class TrapSheep implements Runnable {
+	public static class TrapSheepEntityListener implements Listener {
+		public TrapSheepEntityListener(YiffBukkit plugin) {
+			plugin.getServer().getPluginManager().registerEvents(entityListener, plugin);
+		}
+
+		@EventHandler(priority = EventPriority.MONITOR)
+		public void onEntityDamage(EntityDamageEvent event) {
+			if (event.getCause() != DamageCause.ENTITY_ATTACK)
+				return;
+
+			if (!trapSheeps.contains(event.getEntity()))
+				return;
+
+			final Entity entity = event.getEntity();
+
+			entity.getWorld().strikeLightning(entity.getLocation());
+		}
+	}
+
 	private final YiffBukkit plugin;
 	protected final Sheep sheep;
 
 	private static Set<Entity> trapSheeps = new HashSet<Entity>();
-	private static EntityListener entityListener = null;
+	private static Listener entityListener = null;
 	private final int taskId;
 
 	public TrapSheep(YiffBukkit plugin, Sheep sheep) {
@@ -31,21 +50,7 @@ public class TrapSheep implements Runnable {
 		if (entityListener != null)
 			return;
 
-		entityListener = new EntityListener() {
-			@Override
-			public void onEntityDamage(EntityDamageEvent event) {
-				if (event.getCause() != DamageCause.ENTITY_ATTACK)
-					return;
-
-				if (!trapSheeps.contains(event.getEntity()))
-					return;
-
-				final Entity entity = event.getEntity();
-
-				entity.getWorld().strikeLightning(entity.getLocation());
-			}
-		};
-		plugin.getServer().getPluginManager().registerEvent(Type.ENTITY_DAMAGE, entityListener, Priority.Monitor, plugin);
+		entityListener = new TrapSheepEntityListener(plugin);
 	}
 
 	@Override
