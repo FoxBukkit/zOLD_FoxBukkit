fac467997ee8e0a98547bebdf694fcffc7b95ce0
Doridian Draconia <mriq91@gmail.com>
2012-01-27 08:25:31 +0100

    Fixed pom

--- src/main/java/de/doridian/yiffbukkit/commands/TestCommand.java
+++ src/main/java/de/doridian/yiffbukkit/commands/TestCommand.java
@@ -1,14 +1,14 @@
 package de.doridian.yiffbukkit.commands;
 
+import de.doridian.yiffbukkit.commands.ICommand.Names;
+import de.doridian.yiffbukkit.commands.ICommand.Permission;
 import org.bukkit.entity.Player;
-
-import de.doridian.yiffbukkit.commands.ICommand.*;
 
 @Names("test")
 @Permission("yiffbukkit.useless.test")
 public class TestCommand extends ICommand {
 	@Override
 	public void Run(final Player ply, String[] args, String argStr) {
-		plugin.mcbans.evidence(ply, "doridian", null);
+		plugin.playerHelper.sendYiffcraftClientCommand(ply, 't', "Creeper");
 	}
 }
