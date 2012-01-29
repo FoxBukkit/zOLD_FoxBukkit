07c95198369f94a09683fc73a7098e1bfdf3baa7
Doridian Draconia <mriq91@gmail.com>
2012-01-29 01:54:21 +0100

    Chat console logging refactored and improved a bit more

--- src/main/java/de/doridian/yiffbukkit/commands/MeCommand.java
+++ src/main/java/de/doridian/yiffbukkit/commands/MeCommand.java
@@ -1,12 +1,14 @@
 package de.doridian.yiffbukkit.commands;
 
 import de.doridian.yiffbukkit.YiffBukkitCommandException;
+import de.doridian.yiffbukkit.chat.ChatChannel;
 import de.doridian.yiffbukkit.chat.ChatHelper;
 import de.doridian.yiffbukkit.commands.ICommand.Help;
 import de.doridian.yiffbukkit.commands.ICommand.Names;
 import de.doridian.yiffbukkit.commands.ICommand.Permission;
 import de.doridian.yiffbukkit.commands.ICommand.Usage;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 @Names({"me", "emote"})
 @Help("Well, it's /me, durp")
@@ -14,16 +16,19 @@ import org.bukkit.command.CommandSender;
 @Permission("yiffbukkit.communication.emote")
 public class MeCommand extends ICommand {
 	@Override
-	public void run(CommandSender commandSender, String[] args, String argStr) throws YiffBukkitCommandException {
+	public void Run(Player commandSender, String[] args, String argStr) throws YiffBukkitCommandException {
 		String message = "\u00a77* "+playerHelper.getPlayerTag(commandSender) + commandSender.getDisplayName() + "\u00a77 " + argStr;
 
 		final String conversationTarget = playerHelper.conversations.get(commandSender.getName());
 		if (conversationTarget == null) {
 			final ChatHelper helper = ChatHelper.getInstance();
-			if(helper.getActiveChannel(asPlayer(commandSender)) == helper.OOC) {
-				final String msg = "* " + commandSender.getName() + " " + argStr;
+			final ChatChannel chan = helper.getActiveChannel(commandSender);
+			final String msg = "* " + commandSender.getName() + " " + argStr;
+			if(chan == helper.OOC) {
 				plugin.ircbot.sendToPublicChannel(msg);
 				plugin.sendConsoleMsg(msg, false);
+			} else {
+				plugin.sendConsoleMsg("[" + chan.name + "] " + msg, false);
 			}
 			helper.sendChat(asPlayer(commandSender), message, false);
 		}
