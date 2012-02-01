d80ca84e9c12dbd2b509cf0d9ef139d0ff6b9afd
Doridian Draconia <mriq91@gmail.com>
2012-02-01 17:05:40 +0100

    Ups!

--- src/main/java/de/doridian/yiffbukkit/console/YiffBukkitConsoleCommands.java
+++ src/main/java/de/doridian/yiffbukkit/console/YiffBukkitConsoleCommands.java
@@ -4,7 +4,7 @@ import de.doridian.yiffbukkit.YiffBukkit;
 
 public class YiffBukkitConsoleCommands {
 	public YiffBukkitConsoleCommands(YiffBukkit plugin) {
-		plugin.getCommand("rawlist").setExecutor(new RawListExecutor());
+		plugin.getCommand("list").setExecutor(new RawListExecutor());
 		plugin.getCommand("yb").setExecutor(new YiffBukkitCommandExecutor(plugin));
 	}
 }
