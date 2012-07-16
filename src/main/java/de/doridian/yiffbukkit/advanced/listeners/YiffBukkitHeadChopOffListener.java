df82e1dd04c287e8074574e0cb13835c9b5362ce
Doridian Draconia <mriq91@gmail.com>
2012-07-16 18:06:27 +0200

    Implementing CBukkit a bit more direct to get an Entity by its ID more efficiently.

--- src/main/java/de/doridian/yiffbukkit/advanced/listeners/YiffBukkitHeadChopOffListener.java
+++ src/main/java/de/doridian/yiffbukkit/advanced/listeners/YiffBukkitHeadChopOffListener.java
@@ -1,13 +1,13 @@
 package de.doridian.yiffbukkit.advanced.listeners;
 
 import de.doridian.yiffbukkitsplit.YiffBukkit;
-import gnu.trove.map.hash.TIntObjectHashMap;
 import gnu.trove.set.hash.TIntHashSet;
 import net.minecraft.server.Packet30Entity;
 import net.minecraft.server.Packet34EntityTeleport;
 import net.minecraft.server.Packet35EntityHeadRotation;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.craftbukkit.CraftWorld;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -19,8 +19,6 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.server.Packet;
 import org.bukkit.event.server.PacketListener;
-
-import java.util.List;
 
 public class YiffBukkitHeadChopOffListener extends PacketListener implements Listener {
 	private final static byte CHOPPED_PITCH = (byte)128;
@@ -40,13 +38,11 @@ public class YiffBukkitHeadChopOffListener extends PacketListener implements Lis
 	}
 
 	private TIntHashSet choppedEntities = new TIntHashSet();
-	private TIntObjectHashMap<Entity> entityIDToEntity = new TIntObjectHashMap<Entity>();
 
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onEntityDeath(EntityDeathEvent event) {
 		int eid = event.getEntity().getEntityId();
 		choppedEntities.remove(eid);
-		entityIDToEntity.remove(eid);
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR)
@@ -64,27 +60,8 @@ public class YiffBukkitHeadChopOffListener extends PacketListener implements Lis
 		choppedEntities.add(damagedEntity.getEntityId());
 	}
 
-	private Entity getEntityByID(int eid, World world) {
-		if(entityIDToEntity.contains(eid)) {
-			Entity result = entityIDToEntity.get(eid);
-			if(result != null && result.isValid())
-				return result;
-		}
-
-		List<Entity> entities = world.getEntities();
-		Entity result = null;
-		try {
-			result = entities.get(eid);
-		} catch(Exception e) { }
-		int currentPos = 0;
-		int maxPos = entities.size();
-		while(result == null || result.getEntityId() != eid) {
-			if(currentPos >= maxPos) return null;
-			result = entities.get(currentPos);
-			currentPos++;
-		}
-		entityIDToEntity.put(eid, result);
-		return result;
+	private net.minecraft.server.Entity getEntityByID(int eid, World world) {
+		return ((CraftWorld)world).getHandle().getEntity(eid);
 	}
 
 	@Override
@@ -106,7 +83,7 @@ public class YiffBukkitHeadChopOffListener extends PacketListener implements Lis
 			case 35:
 				Packet35EntityHeadRotation packet35 = (Packet35EntityHeadRotation)packetRaw;
 				if(choppedEntities.contains(packet35.a)) {
-					float yaw = getEntityByID(packet35.a, ply.getWorld()).getLocation().getYaw();
+					float yaw = getEntityByID(packet35.a, ply.getWorld()).yaw;
 					packet35.b = (byte)(((yaw % 360) / 360) * 255);
 				}
 				break;
