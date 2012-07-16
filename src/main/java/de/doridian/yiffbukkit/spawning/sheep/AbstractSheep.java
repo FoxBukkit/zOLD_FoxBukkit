be83501bed7f1386e3256b7d99de0b0870a6674e
TomyLobo <tomylobo@nurfuerspam.de>
2012-07-16 16:46:20 +0200

    Made AbstractSheep use ScheduledTask.

--- src/main/java/de/doridian/yiffbukkit/spawning/sheep/AbstractSheep.java
+++ src/main/java/de/doridian/yiffbukkit/spawning/sheep/AbstractSheep.java
@@ -1,25 +1,24 @@
 package de.doridian.yiffbukkit.spawning.sheep;
 
+import de.doridian.yiffbukkit.main.util.ScheduledTask;
 import de.doridian.yiffbukkitsplit.YiffBukkit;
 import org.bukkit.DyeColor;
 import org.bukkit.entity.Sheep;
 
-public abstract class AbstractSheep implements Runnable {
-	private final YiffBukkit plugin;
+public abstract class AbstractSheep extends ScheduledTask {
 	protected final Sheep sheep;
-	private final int taskId;
 
 	public AbstractSheep(YiffBukkit plugin, Sheep sheep) {
-		this.plugin = plugin;
+		super(plugin);
 		this.sheep = sheep;
 
-		taskId = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, this, 0, 10);
+		scheduleSyncRepeating(0, 10);
 	}
 
 	@Override
 	public void run() {
 		if (sheep.isDead() || sheep.isSheared()) {
-			plugin.getServer().getScheduler().cancelTask(taskId);
+			cancel();
 			return;
 		}
 
