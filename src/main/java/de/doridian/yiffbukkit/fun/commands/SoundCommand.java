b94f6d9f112231de9e6cc6a653a333755809cb07
TomyLobo <tomylobo@nurfuerspam.de>
2012-08-03 15:54:32 +0200

    Added a /sound command.

---
+++ src/main/java/de/doridian/yiffbukkit/fun/commands/SoundCommand.java
@@ -0,0 +1,35 @@
+package de.doridian.yiffbukkit.fun.commands;
+
+import de.doridian.yiffbukkit.main.YiffBukkitCommandException;
+import de.doridian.yiffbukkit.main.commands.ICommand;
+import de.doridian.yiffbukkit.main.commands.ICommand.*;
+import de.doridian.yiffbukkit.main.util.Utils;
+import de.doridian.yiffbukkitsplit.util.PlayerHelper;
+import org.bukkit.entity.Player;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Names("sound")
+@Help("Makes noise")
+@Usage("<volume> <pitch> <sound name>")
+@Permission("yiffbukkit.sound")
+public class SoundCommand extends ICommand {
+	final Pattern argumentPattern = Pattern.compile("^([^ ]+) ([^ ]+) (.+)$");
+
+	@Override
+	public void Run(Player ply, String[] args, String argStr) throws YiffBukkitCommandException {
+		args = parseFlags(args);
+
+		final Matcher matcher = argumentPattern.matcher(argStr);
+		if (!matcher.matches())
+			throw new YiffBukkitCommandException("Invalid arguments.");
+
+		final float volume = Float.parseFloat(matcher.group(1));
+		final float pitch = Float.parseFloat(matcher.group(2));
+		final String soundName = matcher.group(3);
+
+		Utils.makeSound(ply.getLocation(), soundName, volume, pitch);
+
+		PlayerHelper.sendDirectedMessage(ply, "Played sound "+soundName+" at volume "+volume+" and pitch "+pitch+".");
+	}
+}
