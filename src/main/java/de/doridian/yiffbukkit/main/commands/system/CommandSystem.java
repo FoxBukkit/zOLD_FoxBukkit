f94ff99cb66db5cc48e965e16dee64e474c6f10e
TomyLobo <tomylobo@nurfuerspam.de>
2012-08-16 13:06:41 +0200

    Moved CommandSystem.getSubClasses to Utils.

--- src/main/java/de/doridian/yiffbukkit/main/commands/system/CommandSystem.java
+++ src/main/java/de/doridian/yiffbukkit/main/commands/system/CommandSystem.java
@@ -1,27 +1,13 @@
 package de.doridian.yiffbukkit.main.commands.system;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.security.CodeSource;
-import java.security.ProtectionDomain;
-import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.bukkit.command.CommandSender;
 
 import de.doridian.yiffbukkit.main.PermissionDeniedException;
 import de.doridian.yiffbukkit.main.YiffBukkitCommandException;
 import de.doridian.yiffbukkit.main.commands.system.ICommand.Cost;
+import de.doridian.yiffbukkit.main.util.Utils;
 import de.doridian.yiffbukkitsplit.YiffBukkit;
 import de.doridian.yiffbukkitsplit.util.PlayerHelper;
 
@@ -55,7 +41,7 @@ public class CommandSystem {
 	}
 
 	public void scanCommands(String packageName) {
-		for (Class<? extends ICommand> commandClass : getSubClasses(ICommand.class, packageName)) {
+		for (Class<? extends ICommand> commandClass : Utils.getSubClasses(ICommand.class, packageName)) {
 			try {
 				commandClass.newInstance();
 			}
@@ -74,91 +60,6 @@ public class CommandSystem {
 				continue;
 			}
 		}
-	}
-
-	private static <T> List<Class<? extends T>> getSubClasses(Class<T> baseClass, String packageName) {
-		final List<Class<? extends T>> ret = new ArrayList<Class<? extends T>>();
-		final File file;
-		try {
-			final ProtectionDomain protectionDomain = baseClass.getProtectionDomain();
-			final CodeSource codeSource = protectionDomain.getCodeSource();
-			final URL location = codeSource.getLocation();
-			final URI uri = location.toURI();
-			file = new File(uri);
-		}
-		catch (URISyntaxException e) {
-			e.printStackTrace();
-			return ret;
-		}
-		final String[] fileList;
-
-		if (file.isDirectory() || (file.isFile() && !file.getName().endsWith(".jar"))) {
-			String packageFolderName = "/"+packageName.replace('.','/');
-
-			URL url = baseClass.getResource(packageFolderName);
-			if (url == null)
-				return ret;
-
-			File directory = new File(url.getFile());
-			if (!directory.exists())
-				return ret;
-
-			// Get the list of the files contained in the package
-			fileList = directory.list();
-		}
-		else if (file.isFile()) {
-			final List<String> tmp = new ArrayList<String>();
-			final JarFile jarFile;
-			try {
-				jarFile = new JarFile(file);
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-				return ret;
-			}
-
-			Pattern pathPattern = Pattern.compile(packageName.replace('.','/')+"/(.+\\.class)");
-			final Enumeration<JarEntry> entries = jarFile.entries();
-			while (entries.hasMoreElements()) {
-				Matcher matcher = pathPattern.matcher(entries.nextElement().getName());
-				if (!matcher.matches())
-					continue;
-
-				tmp.add(matcher.group(1));
-			}
-
-			fileList = tmp.toArray(new String[tmp.size()]);
-		}
-		else {
-			return ret;
-		}
-
-		Pattern classFilePattern = Pattern.compile("(.+)\\.class");
-		for (String fileName : fileList) {
-			// we are only interested in .class files
-			Matcher matcher = classFilePattern.matcher(fileName);
-			if (!matcher.matches())
-				continue;
-
-			// removes the .class extension
-			String classname = matcher.group(1);
-			try {
-				final String qualifiedName = packageName+"."+classname.replace('/', '.');
-				final Class<?> classObject = Class.forName(qualifiedName);
-				final Class<? extends T> classT = classObject.asSubclass(baseClass);
-
-				// Try to create an instance of the object
-				ret.add(classT);
-			}
-			catch (ClassCastException e) {
-				continue;
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		return ret;
 	}
 
 	public void registerCommand(String name, ICommand command) {
