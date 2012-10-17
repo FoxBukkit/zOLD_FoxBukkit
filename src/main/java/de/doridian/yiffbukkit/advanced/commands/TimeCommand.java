3fa783eb95f024a837174357ac5fb4287b40c097
TomyLobo <tomylobo@nurfuerspam.de>
2012-10-17 14:02:01 +0200

    Added a -s flag to /time, which sets the server time instead of freezing it.

--- src/main/java/de/doridian/yiffbukkit/advanced/commands/TimeCommand.java
+++ src/main/java/de/doridian/yiffbukkit/advanced/commands/TimeCommand.java
@@ -1,5 +1,6 @@
 package de.doridian.yiffbukkit.advanced.commands;
 
+import de.doridian.yiffbukkit.main.PermissionDeniedException;
 import de.doridian.yiffbukkit.main.YiffBukkitCommandException;
 import de.doridian.yiffbukkit.main.commands.system.ICommand.Help;
 import de.doridian.yiffbukkit.main.commands.system.ICommand.Names;
@@ -8,16 +9,44 @@ import de.doridian.yiffbukkit.main.commands.system.ICommand.StringFlags;
 import de.doridian.yiffbukkit.main.commands.system.ICommand.Usage;
 import de.doridian.yiffbukkitsplit.util.PlayerHelper;
 import de.doridian.yiffbukkitsplit.util.PlayerHelper.WeatherType;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 @Names("time")
 @Help("Forces/fixes current time *clientside*.")
 @Usage("[normal|night|day|morning|afternoon|<0-23>]")
-@StringFlags("w")
+@StringFlags("ws")
 @Permission("yiffbukkit.time")
 public class TimeCommand extends ServerTimeCommand {
 	@Override
 	protected void setTime(CommandSender commandSender, Long setTime, Long displayTime, WeatherType setWeather) throws YiffBukkitCommandException {
+		if (booleanFlags.contains('s')) {
+			if (!commandSender.hasPermission("yiffbukkit.time.set"))
+				throw new PermissionDeniedException();
+
+			if (setTime == null)
+				throw new YiffBukkitCommandException("Must provide a time!");
+
+			if (commandSender instanceof Player) {
+				final World world = ((Player) commandSender).getWorld();
+				world.setTime(setTime);
+
+				PlayerHelper.sendDirectedMessage(commandSender, "You set the time on '"+world.getName()+"' to: " + displayTime + ":00");
+			}
+			else {
+				for (World world : Bukkit.getWorlds()) {
+					world.setTime(setTime);
+				}
+
+				PlayerHelper.sendDirectedMessage(commandSender, "You set the time on all worlds to: " + displayTime + ":00");
+			}
+
+
+			return;
+		}
 		if (setTime == null) {
 			playerHelper.resetFrozenServerTime(asPlayer(commandSender));
 			PlayerHelper.sendDirectedMessage(commandSender, "Reset your time back to normal!");
