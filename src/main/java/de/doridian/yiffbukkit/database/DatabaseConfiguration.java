11740a8931b15f9d0c93688bd28f2ce6a1d1e4b3
Doridian Draconia <mriq91@gmail.com>
2013-05-04 14:22:53 +0200

    DB IP change

--- src/main/java/de/doridian/yiffbukkit/database/DatabaseConfiguration.java
+++ src/main/java/de/doridian/yiffbukkit/database/DatabaseConfiguration.java
@@ -3,12 +3,6 @@ package de.doridian.yiffbukkit.database;
 public class DatabaseConfiguration {
 	protected static final String NAME = "doribans";
 	protected static final String USER = "doribans";
-	protected static final String HOST = "193.242.210.116:3306";
+	protected static final String HOST = "10.5.5.10:3306";
 	protected static final String PASSWORD = "SECRET";
-
-    protected static final int MIN_CONNECTIONS = 1;
-    protected static final int MAX_CONNECTIONS = 20;
-    protected static final long TIMEOUT_MS = 10000;
-    protected static final int MIN_SPARE_CONNECTIONS = 1;
-    protected static final int MAX_SPARE_CONNECTIONS = 6;
 }
