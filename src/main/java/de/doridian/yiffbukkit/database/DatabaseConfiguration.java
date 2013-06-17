fdc1c4172df074738f755bb3a6e1bd03824dba27
TomyLobo <tomylobo@nurfuerspam.de>
2013-06-17 15:08:33 +0200

    Fixed db config.

--- src/main/java/de/doridian/yiffbukkit/database/DatabaseConfiguration.java
+++ src/main/java/de/doridian/yiffbukkit/database/DatabaseConfiguration.java
@@ -1,8 +1,8 @@
 package de.doridian.yiffbukkit.database;
 
 public class DatabaseConfiguration {
-	protected static final String NAME = "doribans";
-	protected static final String USER = "doribans";
-	protected static final String HOST = "arcticfox-mc.doridian.de:3306";
+	protected static final String NAME = "mcraft_bans";
+	protected static final String USER = "mcraft_bans";
+	protected static final String HOST = "arcticfox-main.doridian.de:3306";
 	protected static final String PASSWORD = "SECRET";
 }
