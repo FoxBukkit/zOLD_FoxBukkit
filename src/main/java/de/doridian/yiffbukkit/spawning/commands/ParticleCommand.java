da6dfbf22e540cd1c980dbc90647427c0f2d0ed0
TomyLobo <tomylobo@nurfuerspam.de>
2013-07-04 18:58:06 +0200

    Added a temporary /particle command, which can be used to try out particles.

---
+++ src/main/java/de/doridian/yiffbukkit/spawning/commands/ParticleCommand.java
@@ -0,0 +1,23 @@
+package de.doridian.yiffbukkit.spawning.commands;
+
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+import org.bukkit.util.Vector;
+
+import de.doridian.yiffbukkit.main.YiffBukkitCommandException;
+import de.doridian.yiffbukkit.main.commands.system.ICommand;
+import de.doridian.yiffbukkit.main.commands.system.ICommand.Help;
+import de.doridian.yiffbukkit.main.commands.system.ICommand.Names;
+import de.doridian.yiffbukkit.main.commands.system.ICommand.Permission;
+import de.doridian.yiffbukkit.spawning.SpawnUtils;
+
+@Names("particle")
+@Help("Spawns a particle.")
+@Permission("yiffbukkit.particle")
+public class ParticleCommand extends ICommand {
+	@Override
+	public void run(CommandSender commandSender, String[] args, String argStr) throws YiffBukkitCommandException {
+		final Location commandSenderLocation = getCommandSenderLocation(commandSender, true);
+		SpawnUtils.makeParticles(commandSenderLocation, new Vector(0,0,0), 150.0, 200, argStr);
+	}
+}
