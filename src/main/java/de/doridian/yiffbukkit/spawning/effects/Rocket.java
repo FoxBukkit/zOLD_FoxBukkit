6702bf93ba3c016912d907f4bcec574af307c057
TomyLobo <tomylobo@nurfuerspam.de>
2013-07-12 18:29:10 +0200

    Alphabetized mob list and added placeholders for the missing mobs.

--- src/main/java/de/doridian/yiffbukkit/spawning/effects/Rocket.java
+++ src/main/java/de/doridian/yiffbukkit/spawning/effects/Rocket.java
@@ -64,17 +64,34 @@ public class Rocket extends YBEffect.PotionTrail {
 
 	private static Map<EntityType, String> fireworkTypes = new EnumMap<EntityType, String>(EntityType.class);
 	static {
-		fireworkTypes.put(EntityType.SPIDER, "1e1b1b,434343/Type=1/Trail/Fade=b3312c");
-		fireworkTypes.put(EntityType.CREEPER, "41cd34,3b511a,ababab/Type=3/Trail");
-		fireworkTypes.put(EntityType.SQUID, "0,001010/Type=0/Trail");
+		//fireworkTypes.put(EntityType.BAT, "");
+		fireworkTypes.put(EntityType.BLAZE, "ffcc33/Type=2/Trail/Fade=666666,777777,888888,999999");
 		fireworkTypes.put(EntityType.CAVE_SPIDER, "1e1b1b,304343/Type=0/Trail/Fade=b3312c");
+		fireworkTypes.put(EntityType.CHICKEN, "ffffff/Type=4/Trail/Fade=ff1117");
+		fireworkTypes.put(EntityType.COW, "909090,4b3e32,4b3e32,4b3e32/Type=1/Trail/Fade=ffffff");
+		fireworkTypes.put(EntityType.CREEPER, "41cd34,3b511a,ababab/Type=3/Trail");
+		//fireworkTypes.put(EntityType.ENDER_DRAGON, "");
+		fireworkTypes.put(EntityType.ENDERMAN, "5b1e66,a035b2/Type=2/Trail/Fade=a035b2,5b1e66/Flicker");
+		//fireworkTypes.put(EntityType.GHAST, "");
+		//fireworkTypes.put(EntityType.GIANT, "");
+		//fireworkTypes.put(EntityType.IRON_GOLEM, "");
+		//fireworkTypes.put(EntityType.MAGMA_CUBE, "");
+		//fireworkTypes.put(EntityType.MUSHROOM_COW, "");
+		//fireworkTypes.put(EntityType.OCELOT, "");
 		fireworkTypes.put(EntityType.PIG, "f09090/Type=0/Trail/Fade=b3312c");
 		fireworkTypes.put(EntityType.PIG_ZOMBIE, "f09090/Type=2/Trail/Fade=b3312c");
-		fireworkTypes.put(EntityType.ENDERMAN, "5b1e66,a035b2/Type=2/Trail/Fade=a035b2,5b1e66/Flicker");
-		fireworkTypes.put(EntityType.BLAZE, "ffcc33/Type=2/Trail/Fade=666666,777777,888888,999999");
-		fireworkTypes.put(EntityType.CHICKEN, "ffffff/Type=4/Trail/Fade=ff1117");
+		//fireworkTypes.put(EntityType.SHEEP, "");
+		//fireworkTypes.put(EntityType.SILVERFISH, "");
+		//fireworkTypes.put(EntityType.SKELETON, "");
+		//fireworkTypes.put(EntityType.SLIME, "");
+		//fireworkTypes.put(EntityType.SNOWMAN, "");
+		fireworkTypes.put(EntityType.SPIDER, "1e1b1b,434343/Type=1/Trail/Fade=b3312c");
+		fireworkTypes.put(EntityType.SQUID, "0,001010/Type=0/Trail");
+		//fireworkTypes.put(EntityType.VILLAGER, "");
+		//fireworkTypes.put(EntityType.WITCH, "");
+		//fireworkTypes.put(EntityType.WITHER, "");
+		//fireworkTypes.put(EntityType.WOLF, "");
 		fireworkTypes.put(EntityType.ZOMBIE, "00a8a8,00a8a8,43389f,43389f,426832/Trail/Fade=a04000");
-		fireworkTypes.put(EntityType.COW, "909090,4b3e32,4b3e32,4b3e32/Type=1/Trail/Fade=ff1117");
 	}
 
 	@Override
