b00647dd6ac1ffa2d76d4f4d7e65c6e850b21261
TomyLobo <tomylobo@nurfuerspam.de>
2013-09-29 16:51:50 +0200

    Made /lookup print a link to the bans page.
    The broken JSON stuff that was there before is now /mcblookup.

--- src/main/java/de/doridian/yiffbukkit/mcbans/commands/LookupCommand.java
+++ src/main/java/de/doridian/yiffbukkit/mcbans/commands/LookupCommand.java
@@ -24,34 +24,6 @@ public class LookupCommand extends ICommand {
 
 		final String otherName = playerHelper.completePlayerName(args[0], true);
 
-		new Thread() {
-			public void run() {
-				JSONObject lookupret = MCBansUtil.apiQuery("exec=playerLookup&player="+MCBansUtil.URLEncode(otherName));
-				/*
-				 * {"total":2,"reputation":0,
-				 * "local":[],
-				 * "global":["mcbans.com .:. permabanned", "minecraft.digiex.net .:. Griefer (ThePanasonicGriefers)"]
-				 * }
-				 */
-
-				PlayerHelper.sendDirectedMessage(commandSender, "Player \u00a73" + otherName + "\u00a7f has \u00a74" + lookupret.get("total").toString() + " ban(s)\u00a7f and \u00a79"+lookupret.get("reputation").toString()+" REP\u00a7f.");
-
-				final JSONArray banReasonsGlobal = (JSONArray)lookupret.get("global");
-				if (!banReasonsGlobal.isEmpty()) {
-					PlayerHelper.sendDirectedMessage(commandSender, "\u00a74Global bans");
-					for(Object obj : banReasonsGlobal) {
-						PlayerHelper.sendDirectedMessage(commandSender, (String)obj);
-					}
-				}
-
-				final JSONArray banReasonsLocal = (JSONArray)lookupret.get("local");
-				if (!banReasonsLocal.isEmpty()) {
-					PlayerHelper.sendDirectedMessage(commandSender, "\u00a76Local bans");
-					for(Object obj : banReasonsLocal) {
-						PlayerHelper.sendDirectedMessage(commandSender, (String)obj);
-					}
-				}
-			}
-		}.start();
+		commandSender.sendMessage("http://bans.mc.doridian.de/player/" + otherName);
 	}
 }
