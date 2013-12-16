c95919f95d0eda5889460dde78776efa31e4ce39
TomyLobo <tomylobo@nurfuerspam.de>
2013-12-17 00:29:36 +0100

    Switched to the chat link's XML stream output.

--- src/main/java/de/doridian/yiffbukkit/chat/RedisHandler.java
+++ src/main/java/de/doridian/yiffbukkit/chat/RedisHandler.java
@@ -1,9 +1,8 @@
 package de.doridian.yiffbukkit.chat;
 
+import de.doridian.yiffbukkit.main.chat.Parser;
 import de.doridian.yiffbukkit.main.util.Configuration;
 import de.doridian.yiffbukkit.main.util.RedisManager;
-import de.doridian.yiffbukkitsplit.YiffBukkit;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPubSub;
@@ -11,7 +10,7 @@ import redis.clients.jedis.JedisPubSub;
 public class RedisHandler extends JedisPubSub implements Runnable {
 	@Override
 	public void run() {
-		RedisManager.readJedisPool.getResource().subscribe(this, "yiffbukkit:to_server");
+		RedisManager.readJedisPool.getResource().subscribe(this, "yiffbukkit:to_server_xml");
 	}
 
 	public static void sendMessage(final Player player, final String  message) {
@@ -29,19 +28,22 @@ public class RedisHandler extends JedisPubSub implements Runnable {
 	@Override
 	public void onMessage(final String channel, final String c_message) {
 		try {
-			//SERVER|USER|MESSAGE
-			final String[] split = c_message.split("\\|", 3);
+			final String[] split = c_message.split("\0", 4);
+
+			// SERVER\0 USER\0 format\0 param1\0 param2
 			final String server = split[0];
-			final Player user = YiffBukkit.instance.playerHelper.matchPlayerSingle(split[1], true);
-			String message = split[2];
-			if(!server.equals(Configuration.getValue("server-name", "Main"))) {
-				message = "\u00a72[" + server + "]\u00a7f " + message;
-				Bukkit.broadcastMessage(message);
-				return;
+			@SuppressWarnings("UnusedDeclaration")
+			final String userName = split[1];
+			String format = split[2];
+			final Object[] params = split[3].split("\0");
+
+			if (!server.equals(Configuration.getValue("server-name", "Main"))) {
+				format = "<color name=\"dark_green\">[" + server + "]</color> " + format;
 			}
-			ChatHelper chatHelper = ChatHelper.getInstance();
-			chatHelper.sendChat(user, message, false, chatHelper.DEFAULT);
-		} catch (Exception e) {
+
+			Parser.sendToAll(format, params);
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
