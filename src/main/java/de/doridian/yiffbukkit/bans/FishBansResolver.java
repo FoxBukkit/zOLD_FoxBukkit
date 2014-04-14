2179a55acfd5fee4eed978527d601e5fe88d2354
Doridian Draconia <mriq91@gmail.com>
2014-04-14 18:05:49 +0200

    Fix UUIDs from Mojang

--- src/main/java/de/doridian/yiffbukkit/bans/FishBansResolver.java
+++ src/main/java/de/doridian/yiffbukkit/bans/FishBansResolver.java
@@ -45,7 +45,10 @@ public class FishBansResolver {
 		try {
 			Profile[] profiles = HTTP_PROFILE_REPOSITORY.findProfilesByNames(username);
 			if(profiles.length == 1) {
-				ret = UUID.fromString(profiles[0].getId());
+				String uuidStr = profiles[0].getId();
+				if(uuidStr.indexOf('-') < 1)
+					uuidStr = uuidStr.substring(0, 8) + "-" + uuidStr.substring(8, 12) + "-" + uuidStr.substring(12, 16) + "-" + uuidStr.substring(16, 20) + "-" + uuidStr.substring(20);
+				ret = UUID.fromString(uuidStr);
 				playerUUIDMap.put(username.toLowerCase(), ret);
 				return ret;
 			}
