6a451259627ea77295f6048d2c5111fca991bd36
Doridian Draconia <mriq91@gmail.com>
2014-05-07 00:27:22 +0200

    Made .clear throw exception

--- src/main/java/de/doridian/yiffbukkit/main/util/RedisManager.java
+++ src/main/java/de/doridian/yiffbukkit/main/util/RedisManager.java
@@ -141,11 +141,7 @@ public class RedisManager {
 
 		@Override
 		public void clear() {
-			for(JedisPool writeJedisPool : writeJedisPools) {
-				Jedis jedis = writeJedisPool.getResource();
-				jedis.del(name);
-				writeJedisPool.returnResource(jedis);
-			}
+			throw new RuntimeException();
 		}
 	}
 
