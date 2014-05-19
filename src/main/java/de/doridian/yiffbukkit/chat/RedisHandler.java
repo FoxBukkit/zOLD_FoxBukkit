7518ca1dab93759008863c2f92061fb14bf7a207
Doridian Draconia <mriq91@gmail.com>
2014-05-19 19:31:44 +0200

    New JSON message handling

--- src/main/java/de/doridian/yiffbukkit/chat/RedisHandler.java
+++ src/main/java/de/doridian/yiffbukkit/chat/RedisHandler.java
@@ -1,16 +1,21 @@
 package de.doridian.yiffbukkit.chat;
 
+import com.google.gson.Gson;
 import de.doridian.dependencies.redis.AbstractRedisHandler;
 import de.doridian.dependencies.redis.RedisManager;
+import de.doridian.yiffbukkit.chat.json.ChatMessage;
 import de.doridian.yiffbukkit.core.YiffBukkit;
 import de.doridian.yiffbukkit.main.chat.Parser;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 public class RedisHandler extends AbstractRedisHandler {
 	public RedisHandler() {
-		super("yiffbukkit:to_server_xml");
+		super("yiffbukkit:to_server");
 	}
 
 	public static void sendMessage(final Player player, final String  message) {
@@ -19,25 +24,41 @@ public class RedisHandler extends AbstractRedisHandler {
 		RedisManager.publish("yiffbukkit:from_server", YiffBukkit.instance.configuration.getValue("server-name", "Main") + "|" + player.getUniqueId() + "|" + player.getName() + "|" + message);
 	}
 
+	private final Gson gson = new Gson();
+
 	@Override
 	public void onMessage(final String c_message) {
 		try {
-			final String[] split = c_message.split("\0", 5);
-
-			// SERVER\0 UUID\0 NAME\0 format\0 param1\0 param2
-			final String server = split[0];
-			@SuppressWarnings("UnusedDeclaration")
-			final UUID userUUID = UUID.fromString(split[1]);
-			@SuppressWarnings("UnusedDeclaration")
-			final String userName = split[2];
-			String format = split[3];
-			final Object[] params = split[4].split("\0");
-
-			if (!server.equals(YiffBukkit.instance.configuration.getValue("server-name", "Main"))) {
-				format = "<color name=\"dark_green\">[" + server + "]</color> " + format;
+			final ChatMessage chatMessage;
+			synchronized (gson) {
+				chatMessage = gson.fromJson(c_message, ChatMessage.class);
 			}
 
-			Parser.sendToAll(format, params);
+			if (!chatMessage.server.equals(YiffBukkit.instance.configuration.getValue("server-name", "Main"))) {
+				chatMessage.contents.xml_format = "<color name=\"dark_green\">[" + chatMessage.server + "]</color> " + chatMessage.contents.xml_format;
+			}
+
+			List<Player> allPlayers = Arrays.asList(YiffBukkit.instance.getServer().getOnlinePlayers());
+			List<Player> targetPlayers = new ArrayList<>();
+			switch(chatMessage.to.type) {
+				case "all":
+					targetPlayers = allPlayers;
+					break;
+				case "permission":
+					for(String permission : chatMessage.to.filter)
+						for (Player player : allPlayers)
+							if (player.hasPermission(permission) && !targetPlayers.contains(player))
+								targetPlayers.add(player);
+					break;
+				case "player":
+					for(String playerUUID : chatMessage.to.filter)
+						for (Player player : allPlayers)
+							if (player.getUniqueId().equals(UUID.fromString(playerUUID)) && !targetPlayers.contains(player))
+								targetPlayers.add(player);
+					break;
+			}
+
+			Parser.sendToPlayers(targetPlayers, chatMessage.contents.xml_format, chatMessage.contents.xml_format_args);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
